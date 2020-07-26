d25035604589c666eb5a07da807b776d0d78c7f8
crundberg <christoffer@crwebb.se>
2020-07-26 23:27:26 +0200

    Started on tests for players

--- src/components/players/__tests__/players.js
+++ src/components/players/__tests__/players.js
@@ -1,8 +1,74 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render, fireEvent } from '@testing-library/react';
+import { Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
+import Provider from 'store';
 import Players from '../players';
 
+const setup = () => {
+	const history = createMemoryHistory();
+	const utils = render(
+		<Provider>
+			<Router history={history}>
+				<Players />
+			</Router>
+		</Provider>
+	);
+
+	const btnAdd = utils.getByText('+');
+	const btnHcp = utils.getByText('Handicap');
+	const inputName = utils.getByLabelText('player');
+
+	return {
+		btnAdd,
+		btnHcp,
+		inputName,
+		...utils,
+	}
+  }
+
 it('renders without crashing', () => {
-	const div = document.createElement('div');
-	ReactDOM.render(<Players />, div);
+	setup();
 });
+
+it('is not possible to add player without name', () => {
+	const { btnAdd, inputName } = setup();
+
+	expect(btnAdd).toBeDisabled();
+	fireEvent.change(inputName, { target: { value: 'CR' } });
+	expect(btnAdd).toBeEnabled();
+});
+
+it('is not possible to add player is name alredy exists', () => {
+	const { btnAdd, inputName } = setup();
+
+	expect(btnAdd).toBeDisabled();
+	fireEvent.change(inputName, { target: { value: 'CR' } });
+	expect(btnAdd).toBeEnabled();
+	fireEvent.click(btnAdd);
+	expect(btnAdd).toBeDisabled();
+	fireEvent.change(inputName, { target: { value: 'C' } });
+	expect(btnAdd).toBeEnabled();
+	fireEvent.change(inputName, { target: { value: 'CR' } });
+	expect(btnAdd).toBeDisabled();
+});
+
+it('is possible to toggle handicap', () => {
+	const { btnHcp } = setup();
+
+	expect(btnHcp).toHaveClass('btn-outline-secondary');
+	fireEvent.click(btnHcp);
+	expect(btnHcp).toHaveClass('btn-secondary');
+	fireEvent.click(btnHcp);
+	expect(btnHcp).toHaveClass('btn-outline-secondary');
+});
+
+it.skip('is possible to add player', () => {
+	const { btnAdd, inputName } = setup();
+
+	fireEvent.change(inputName, { target: { value: 'CR' } });
+	fireEvent.click(btnAdd);
+	expect(inputName.value).toBe('');
+});
+
+it('is possible to add player with handicap', () => {});
