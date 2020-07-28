5478af9416bded4298366f55a4e2a540255c4756
crundberg <christoffer@crwebb.se>
2020-07-28 21:29:04 +0200

    Fixed prettier

--- src/components/game/game.js
+++ src/components/game/game.js
@@ -1,14 +1,14 @@
 import React, { useState } from 'react';
-import { Redirect } from "react-router-dom";
-import { usePlayers } from 'hooks'
-import classNames from 'classnames'
+import { Redirect } from 'react-router-dom';
+import { usePlayers } from 'hooks';
+import classNames from 'classnames';
 
 function Game() {
-	const [ points, setPoints ] = useState(-1);
+	const [points, setPoints] = useState(-1);
 	const { players, handleAddPoint, handleNewGame } = usePlayers();
 
 	if (players.length === 0) {
-		return <Redirect to="/" />
+		return <Redirect to="/" />;
 	}
 
 	const playersTurn = players.reduce((turn, player) => {
@@ -18,7 +18,8 @@ function Game() {
 		if (player.disqualified) return turn;
 		else if (turnLength > playerLength) return player;
 		else if (turnLength < playerLength) return turn;
-		else if (player.points[turnLength - 1] < turn.points[playerLength - 1]) return player;
+		else if (player.points[turnLength - 1] < turn.points[playerLength - 1])
+			return player;
 
 		return turn;
 	}, players[0]);
@@ -26,11 +27,11 @@ function Game() {
 	const handlePointEvent = () => {
 		handleAddPoint(playersTurn.name, points);
 		setPoints(-1);
-	}
+	};
 
 	const sort = (a, b) => {
 		var comparison = 0;
-	
+
 		if (!a.disqualified && b.disqualified) {
 			comparison = -1;
 		} else if (a.disqualified && !b.disqualified) {
@@ -40,59 +41,88 @@ function Game() {
 		} else if (a.currentPoints < b.currentPoints) {
 			comparison = 1;
 		}
-	
+
 		return comparison;
-	}
+	};
 
 	const pointButton = (_e, i) => {
 		const className = classNames('btn btn-lg mr-1 mb-1', {
 			'btn-primary': points === i,
 			'btn-outline-primary': points !== i,
-		})
+		});
 
-		return <button type="button" className={className} onClick={() => setPoints(i)} key={i}>{i}</button>
-	}
+		return (
+			<button
+				type="button"
+				className={className}
+				onClick={() => setPoints(i)}
+				key={i}
+			>
+				{i}
+			</button>
+		);
+	};
 
 	return (
 		<div className="container">
 			<div className="card">
 				<div className="card-header">
 					CR Mölkky
-
 					<div className="float-right">
-						<button type="button" className="btn btn-primary btn-sm mr-1" onClick={() => handleNewGame()}>New game</button>
-						<button type="button" className="btn btn-primary btn-sm" onClick={() => handleNewGame()}>Settings</button>
+						<button
+							type="button"
+							className="btn btn-primary btn-sm mr-1"
+							onClick={() => handleNewGame()}
+						>
+							New game
+						</button>
+						<button
+							type="button"
+							className="btn btn-primary btn-sm"
+							onClick={() => handleNewGame()}
+						>
+							Settings
+						</button>
 					</div>
 				</div>
 				<div className="card-body">
 					<ul className="list-group mb-3">
-						{players.sort(sort).map(player => {
+						{players.sort(sort).map((player) => {
 							const className = classNames(
 								'list-group-item d-flex justify-content-between align-items-center',
 								{
-									'active': player.name === playersTurn.name && !player.winner,
-									'disabled': player.disqualified,
-									'list-group-item-warning': player.winner
+									active: player.name === playersTurn.name && !player.winner,
+									disabled: player.disqualified,
+									'list-group-item-warning': player.winner,
 								}
 							);
 
-							return <li className={className} key={player.name}>
-								{player.name}
-								<span className="badge badge-danger badge-pill">{player.handicap ? 'HCP' : player.misses}</span>
-								<span className="badge badge-primary badge-pill">{player.currentPoints}</span>
-							</li>
+							return (
+								<li className={className} key={player.name}>
+									{player.name}
+									<span className="badge badge-danger badge-pill">
+										{player.handicap ? 'HCP' : player.misses}
+									</span>
+									<span className="badge badge-primary badge-pill">
+										{player.currentPoints}
+									</span>
+								</li>
+							);
 						})}
 					</ul>
 
 					<h5 className="card-title">{playersTurn.name}&apos;s turn</h5>
 
-					<div className="mb-3">
-						{[...Array(13)].map(pointButton)}
-					</div>
+					<div className="mb-3">{[...Array(13)].map(pointButton)}</div>
 
 					<p>
-						<button type="button" className="btn btn-primary" onClick={() => handlePointEvent()} disabled={points < 0}>
-							{ (points > 0) ? `OK (+${points})` : 'Missed' }
+						<button
+							type="button"
+							className="btn btn-primary"
+							onClick={() => handlePointEvent()}
+							disabled={points < 0}
+						>
+							{points > 0 ? `OK (+${points})` : 'Missed'}
 						</button>
 					</p>
 				</div>
