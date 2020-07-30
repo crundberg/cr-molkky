e852bddae777e51ccb405879cbd0968df7eeba51
crundberg <christoffer@crwebb.se>
2020-07-31 00:03:40 +0200

    Changed to hashrouter to support Github

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 import { Start } from 'components/start';
 import { Players } from 'components/players';
 import { Game } from 'components/game';
