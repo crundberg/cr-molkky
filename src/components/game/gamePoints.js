7a8bf73706e635b7c68e14eb4111ec6ddd377ba8
crundberg <christoffer@crwebb.se>
2020-08-06 22:17:19 +0200

    Fixed game points bug when game was complete.

--- src/components/game/gamePoints.js
+++ src/components/game/gamePoints.js
@@ -31,29 +31,27 @@ function Game() {
 		);
 	};
 
-	return (
-		playersTurn.name && (
-			<div className="card-body">
-				<div className="points">
-					<h5 className="card-title">{playersTurn.name}&apos;s turn</h5>
+	return playersTurn.name ? (
+		<div className="card-body">
+			<div className="points">
+				<h5 className="card-title">{playersTurn.name}&apos;s turn</h5>
 
-					<div className="mb-3">{[...Array(13)].map(pointButton)}</div>
+				<div className="mb-3">{[...Array(13)].map(pointButton)}</div>
 
-					<p>
-						<button
-							type="button"
-							className="btn btn-primary"
-							onClick={() => handlePointEvent()}
-							disabled={points < 0}
-							data-testid="btnAdd"
-						>
-							{points > 0 ? `OK (+${points})` : 'Missed'}
-						</button>
-					</p>
-				</div>
+				<p>
+					<button
+						type="button"
+						className="btn btn-primary"
+						onClick={() => handlePointEvent()}
+						disabled={points < 0}
+						data-testid="btnAdd"
+					>
+						{points > 0 ? `OK (+${points})` : 'Missed'}
+					</button>
+				</p>
 			</div>
-		)
-	);
+		</div>
+	) : null;
 }
 
 export default Game;
