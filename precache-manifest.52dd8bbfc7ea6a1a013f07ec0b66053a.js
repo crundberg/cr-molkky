0a3506311d6f2e191ab3e8006c89b283bbba119d
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-08-25 10:11:20 +0000

    Deploy crundberg/cr-molkky to github.com/crundberg/cr-molkky.git:gh-pages

---
+++ precache-manifest.52dd8bbfc7ea6a1a013f07ec0b66053a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37ee790424bd988c434a25d404eacf2b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cda0e00709d4a444a218",
+    "url": "/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "1b994b34c0fe78105a49",
+    "url": "/static/css/main.0f37c994.chunk.css"
+  },
+  {
+    "revision": "cda0e00709d4a444a218",
+    "url": "/static/js/2.5271efc7.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.5271efc7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b994b34c0fe78105a49",
+    "url": "/static/js/main.166941f9.chunk.js"
+  },
+  {
+    "revision": "7dfa776dd87c66b51788",
+    "url": "/static/js/runtime-main.8d9a5b94.js"
+  }
+]);
